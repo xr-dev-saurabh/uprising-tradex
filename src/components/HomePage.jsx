a5c362c4ec3f329f9c6f22cf24fd6f2eff239494
unknown <abel@vamverse.com>
2023-06-14 11:17:48 +0100

    responsive navbar with formatted

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,25 +1,30 @@
-import play from "../assets/images/play-circle.png"
-import bgImage from "../assets/images/navBg.png"
+import play from "../assets/images/play-circle.png";
+// import bgImage from "../assets/images/navBg.png";
 
 const HomePage = () => {
+  return (
+    <div className="relative   flex items-center  font-inter">
+      <div className="w-[337.76px] sm:w-[400px]  sm:h-[256px] h-[224.05px]  ml-7 sm:ml-24">
+        <h3 className="sm:text-[53px] text-[45px] w-full h-[123px] sm:h-[146px]  font-extrabold">
+          Workspace that inspire
+        </h3>
+        <p className="h-[48px] w-full text-[11px] sm:text-[13px] font-normal leading-6">
+          We work for people and make their dream come in to there life or in
+          there home.
+        </p>
+        <div className="w-[175px] h-[36px]  flex items-center justify-between">
+          <img className="cursor-pointer" src={play} alt="play" />
+          <div className="line w-[59px] bg-[#18191f] border-[1.48px] border-[#18191f]"></div>
 
-    return (
-
-           <div className="relative   flex items-center  font-inter">
-                <div className="w-[337.76px] sm:w-[400px]  sm:h-[256px] h-[224.05px]  ml-7 sm:ml-24">
-                    <h3 className="sm:text-[53px] text-[45px] w-full h-[123px] sm:h-[146px]  font-extrabold">Workspace that inspire</h3>
-                    <p className="h-[48px] w-full text-[11px] sm:text-[13px] font-normal leading-6">We work for people and make their dream come in to there life or in there home.</p>
-                    <div className="w-[175px] h-[36px]  flex items-center justify-between">
-                        <img className="cursor-pointer" src={play} alt="play"/>
-                        <div className="line w-[59px] bg-[#18191f] border-[1.48px] border-[#18191f]"></div>
-                        <p className="h-[14px] w-[68px] text-[12px] font-bold leading-[13px] tracking-[0.7px]">DISCOVER</p>
-                    </div>
-                </div>
-                {/* <div className="bg-image ml-auto flex items-end justify-end">
+          <p className="h-[14px] w-[68px] text-[12px] font-bold leading-[13px] tracking-[0.7px]">
+            DISCOVER
+          </p>
+        </div>
+      </div>
+      {/* <div className="bg-image ml-auto flex items-end justify-end">
                     <img className="h-[500px]  w-[200px] sm:w-[520px] " src={bgImage} alt="backgroundImage"/>
                 </div> */}
-           </div>
-        
-    )
-}
+    </div>
+  );
+};
 export default HomePage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,31 @@
-import logo from '../assets/images/logo.png';
-
-
+import logo from "../assets/images/logo.png";
 
 const Navbar = () => {
-   
-    return (
-        <div className='ml-7 mt-4 sm:flex flex-wrap items-center w-10/12 relative'>
-                <div className="logo w-[180px]  flex flex-row items-center relative cursor-pointer">
-                    <img className='w-[44px] sm:w-[67px] h-[32px] sm:h-[48px]' src={logo} alt='brand-logo' />
-                    <p className='sm:h-[28px] h-[18px] sm:w-[105px] w-[70px]  font-roboto sm:text-[24px] text-[16px] font-normal sm:leading-7 leading-[18px] sm:m-0 my-auto absolute sm:top-[20px] bottom-0 top-[0px] sm:left-[75px] left-[50px] '>UPRISING</p>
-                </div>
-                <input type='checkbox' id='check' className='hidden'/>
-                <label for="check" className=' text-[#18191f] text-3xl leading-[80px]  sm:hidden cursor-pointer'>
-                <i class="fa-solid fa-bars absolute top-0  right-0"></i>
-                </label>
-                    <ul className='ml-auto font-roboto sm:flex justify-center items-center left-[-100%] mt-5 sm:mt-0 text-[14px] sm:text-[18px] gap-x-8 text-center h-[100vh] max-h-0 sm:h-0 font-light leading-[21px]  custom:overflow-hidden transition-[max-height] duration-500 ease-in-out'>
-                        <li className='cursor-pointer'>HOME</li>
-                        <li className='cursor-pointer'>SERVICES</li>
-                        <li className='cursor-pointer'>ABOUT</li>
-                    </ul>
-        </div>
-    )
-}
+  return (
+    <div className="ml-7 mt-4 sm:flex flex-wrap items-center w-10/12 relative">
+      <div className="logo w-[180px]  flex flex-row items-center relative cursor-pointer">
+        <img
+          className="w-[44px] sm:w-[67px] h-[32px] sm:h-[48px]"
+          src={logo}
+          alt="brand-logo"
+        />
+        <p className="sm:h-[28px] h-[18px] sm:w-[105px] w-[70px]  font-roboto sm:text-[24px] text-[16px] font-normal sm:leading-7 leading-[18px] sm:m-0 my-auto absolute sm:top-[20px] bottom-0 top-[0px] sm:left-[75px] left-[50px] ">
+          UPRISING
+        </p>
+      </div>
+      <input type="checkbox" id="check" className="hidden" />
+      <label
+        for="check"
+        className=" text-[#18191f] text-3xl leading-[80px]  sm:hidden cursor-pointer"
+      >
+        <i class="fa-solid fa-bars absolute top-0 right-0"></i>
+      </label>
+      <ul className="ml-auto font-roboto sm:flex justify-center items-center left-[-100%] mt-5 sm:mt-0 text-[14px] sm:text-[18px] gap-x-8 text-center h-[100vh] max-h-0 sm:h-0 font-light leading-[21px]  custom:overflow-hidden transition-[max-height] duration-500 ease-in-out">
+        <li className="cursor-pointer">HOME</li>
+        <li className="cursor-pointer">SERVICES</li>
+        <li className="cursor-pointer">ABOUT</li>
+      </ul>
+    </div>
+  );
+};
 export default Navbar;
