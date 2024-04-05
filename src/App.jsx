0644c46468e4cb699059a6f9621837663424c2a7
ashishjha089 <ajha91591@gmail.com>
2024-04-05 19:46:10 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
+import Navbar from "./components/Navbar";
+import CardIct from "./components/CardIct";
+import WhyUs from "./components/WhyUs";
+import Footer from "./components/Footer";
 
-
-import Navbar from "./components/Navbar"
-import CardIct from "./components/CardIct"
-import WhyUs from './components/WhyUs'
- import Footer from './components/Footer'
 function App() {
-  
-
   return (
     <>
-      <Navbar/> 
-      <CardIct/>
-      <WhyUs/>
-       <Footer/> 
+      <Navbar />
+      <CardIct />
+      <WhyUs />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
