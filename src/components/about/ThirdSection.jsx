233bb954229262431520dd5bc9e272cabb622996
AzadNishad <azadnishad9801@gmail.com>
2024-04-08 14:39:45 +0530

    adjust responsive

--- src/components/about/ThirdSection.jsx
+++ src/components/about/ThirdSection.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import Fancy3 from '../../assets/Rectangle 48.png';
-import Fancy4 from '../../assets/Rectangle 49.png';
-import Fancy5 from '../../assets/Rectangle 50.png';
+import TeamImg from '../../assets/team.jpg';
 import WhyUs from '../../assets/why-us.png';
+import WhyUsMobile from '../../assets/whyus-mobile.png';
 
 export default function ThirdSection() {
     return (
-        <div className='bg-[#fbf9f9]'>
+        <div className='bg-[#f8f9fb]'>
             <div className='p-5 lg:p-24'>
-                <div className='flex flex-col items-center  lg:flex-row justify-between '>
-                    <div className='flex items-center justify-center lg:justify-start  gap-10 w-full lg:w-[35%]'>
-                        <div className='flex flex-col w-[10rem] gap-5'>
-                            <img src={Fancy3} alt="" className='' />
-                            <img src={Fancy5} alt="" />
+                <div className=' '>
+                    <h1 className='text-3xl lg:text-5xl font-serif uppercase text-center mb-10 lg:mb-20 mt-10 lg:mt-0'>Meet Our Dynamic Team</h1>
+                    <div className='xl:flex  justify-between gap-5'>
+                        <div className='mb-5 w-full xl:w-[25%]'>
+                            <img src={TeamImg} alt="" />
                         </div>
-                        <img src={Fancy4} alt="" />
-                    </div>
-                    <div className='lg:w-[60%]'>
-                        <h1 className='text-3xl lg:text-5xl uppercase text-center mb-10 lg:mb-20 mt-10 lg:mt-0'>Meet Our Dynamic Team</h1>
-                        <p className='text-lg text-justify'>
+                        <p className='text-lg text-justify xl:w-[75%]'>
                             At Uprising, our success is driven by our dedicated team of professionals who are passionate about delivering excellence in everything they do. We pride ourselves on having a diverse and talented team that drives innovation and excellence in everything we do. From machine learning engineers who harness the power of AI to cloud engineers who ensure the reliability and scalability of our infrastructure, each member brings a unique set of skills and expertise to the table. Our extended reality developers push the boundaries of technology, creating immersive experiences that captivate and inspire. Meanwhile, our 3D modelers bring creativity and precision to every project, crafting lifelike visuals that leave a lasting impression.
                             <br />
                             <br />
@@ -27,7 +22,9 @@ export default function ThirdSection() {
                     </div>
                 </div>
             </div>
-            <img src={WhyUs} alt="" className='px-1 lg:px-24 w-full' />
+            <img src={WhyUs} alt="" className='px-1 lg:px-24 w-full hidden sm:block' />
+            <img src={WhyUsMobile} alt="" className='px-1 lg:px-24 w-full py-10 sm:hidden' />
+
         </div>
     )
 }
