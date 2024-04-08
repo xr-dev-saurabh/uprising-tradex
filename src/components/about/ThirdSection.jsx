498ba5682eaefda851066201b461341c156a3628
AzadNishad <azadnishad9801@gmail.com>
2024-04-08 13:36:53 +0530

    growth spelling change

--- src/components/about/ThirdSection.jsx
+++ src/components/about/ThirdSection.jsx
@@ -22,7 +22,7 @@ export default function ThirdSection() {
                             At Uprising, our success is driven by our dedicated team of professionals who are passionate about delivering excellence in everything they do. We pride ourselves on having a diverse and talented team that drives innovation and excellence in everything we do. From machine learning engineers who harness the power of AI to cloud engineers who ensure the reliability and scalability of our infrastructure, each member brings a unique set of skills and expertise to the table. Our extended reality developers push the boundaries of technology, creating immersive experiences that captivate and inspire. Meanwhile, our 3D modelers bring creativity and precision to every project, crafting lifelike visuals that leave a lasting impression.
                             <br />
                             <br />
-                            In addition, our AR filter developers create engaging experiences for social media platforms, while our web developers and UI/UX designers work tirelessly to deliver seamless and intuitive digital experiences for our customers. Together, our team collaborates seamlessly to deliver innovative solutions that exceed expectations and drive business growt
+                            In addition, our AR filter developers create engaging experiences for social media platforms, while our web developers and UI/UX designers work tirelessly to deliver seamless and intuitive digital experiences for our customers. Together, our team collaborates seamlessly to deliver innovative solutions that exceed expectations and drive business growth.
                         </p>
                     </div>
                 </div>
