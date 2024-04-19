10321f9bbf6fdc1f49648eda843283198a6e7031
saurabh <saurabhu154@gmail.com>
2024-04-19 12:36:15 +0530

    added changes for Components name  in app.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import BigCard from "./components/BigCard";
-import { Hero } from "./components/Hero";
-import Misson from "./components/Misson";
-import Servies from "./components/Servies";
-import Navbar from "./components/Navbar";
-import CardIct from "./components/CardIct";
-import WhyUs from "./components/WhyUs";
-import Footer from "./components/Footer";
-import FirstSection from "./components/about/FirstSection";
-import SecondSection from "./components/about/SecondSection";
-import ThirdSection from "./components/about/ThirdSection";
-import ProductHero from "./components/product/ProductHero";
-import ProductsRow from "./components/product/ProductsRow";
-import NavOrder from "./components/NavOrder";
+import BigCard from "./Components/BigCard";
+import { Hero } from "./Components/Hero";
+import Misson from "./Components/Misson";
+import Servies from "./Components/Servies";
+import Navbar from "./Components/Navbar";
+import CardIct from "./Components/CardIct";
+import WhyUs from "./Components/WhyUs";
+import Footer from "./Components/Footer";
+import FirstSection from "./Components/about/FirstSection";
+import SecondSection from "./Components/about/SecondSection";
+import ThirdSection from "./Components/about/ThirdSection";
+import ProductHero from "./Components/product/ProductHero";
+import ProductsRow from "./Components/product/ProductsRow";
+import NavOrder from "./Components/NavOrder";
 
 export default function App() {
   return (
@@ -63,10 +63,3 @@ export default function App() {
     </>
   );
 }
-
-//Steps for route
-//1. Landing Page has all the above components. Navbar has two components, Navbar and ModelHam, add link for route in both.
-// On clicking on Contact button on navbar, it should take the user to footer section with smooth scroll like in ProductHero component.
-//2. Second is About page which has First, Second and ThirdSection in about folder, it should have footer and should re-direct to footer on onclick Contact button.
-//Footer must be in all pages and navbar in all pages.
-//3. Third is Product page which has ProductHero and ProductRow with footer. Add this to Order button on navbar and Modalham
